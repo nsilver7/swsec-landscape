f695b9a7c8cb2719a6a2475cb3d1d5ee3d3de232
Nick <nicksilver@gmail.com>
2026-05-15 13:05:24 -0500

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // For GitHub Pages: set to '/<repo-name>/' or '/' if using a custom domain
   base: command === "serve" ? "/" : "/swsec-landscape/",
-});
+}));
